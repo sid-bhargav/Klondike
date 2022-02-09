66ec4fe5481d6705a8172aa5fdb0a422272097c4
Siddharth Bhargav <sid.bhargav@gmail.com>
2022-02-09 09:28:27 -0800

    Added location instance variables to Card

--- src/Card.java
+++ src/Card.java
@@ -4,6 +4,10 @@ import java.awt.event.ActionEvent;
 /** represents a playing card that can draw itself. */
 public class Card implements Drawable, Updateable{
 
+    // Card location coordinates
+    private int x;
+    private int y;
+
     @Override
     public void update(ActionEvent a) {
         // TODO Auto-generated method stub
