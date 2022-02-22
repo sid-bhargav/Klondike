8533e7cf987001ff9b18344021ef76465b60d7e3
Siddharth Bhargav <sid.bhargav@gmail.com>
2022-02-22 08:44:54 -0800

    Add some code to Inplay

---
+++ src/InPlay.java
@@ -0,0 +1,24 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+
+public class InPlay extends Pile {
+
+    @Override
+    public void draw(Graphics g) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void update(ActionEvent a) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean canAddCard(Card c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+}
